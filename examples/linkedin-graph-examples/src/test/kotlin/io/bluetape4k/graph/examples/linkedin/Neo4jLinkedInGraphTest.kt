919a9b34704e5ad2f90a83cd0cc67bd84d5f3ad4
debop <sunghyouk.bae@gmail.com>
2026-04-18 20:52:30 +0900

    refactor(linkedin-graph-examples): migrate tests to bluetape4k-testcontainers graphdb

--- examples/linkedin-graph-examples/src/test/kotlin/io/bluetape4k/graph/examples/linkedin/Neo4jLinkedInGraphTest.kt
+++ examples/linkedin-graph-examples/src/test/kotlin/io/bluetape4k/graph/examples/linkedin/Neo4jLinkedInGraphTest.kt
@@ -1,14 +1,14 @@
 package io.bluetape4k.graph.examples.linkedin
 
 import io.bluetape4k.graph.neo4j.Neo4jGraphOperations
-import io.bluetape4k.graph.servers.Neo4jServer
+import io.bluetape4k.testcontainers.graphdb.Neo4jServer
 import org.junit.jupiter.api.AfterAll
 import org.neo4j.driver.AuthTokens
 import org.neo4j.driver.Driver
 import org.neo4j.driver.GraphDatabase
 
 class Neo4jLinkedInGraphTest: AbstractLinkedInGraphTest() {
-    private val driver: Driver = GraphDatabase.driver(Neo4jServer.instance.boltUrl, AuthTokens.none())
+    private val driver: Driver = GraphDatabase.driver(Neo4jServer.Launcher.neo4j.boltUrl, AuthTokens.none())
     override val ops = Neo4jGraphOperations(driver)
 
     @AfterAll
