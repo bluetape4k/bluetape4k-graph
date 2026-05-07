4ab7b695fc2c7ac86362c38ec92df85a463d0ba5
debop <sunghyouk.bae@gmail.com>
2026-05-07 22:56:27 +0900

    Revert "fix: SNAPSHOT 의존성 캐시 비활성화 (0초) - CI 캐시 오염 방지"
    This reverts commit f2653fa4d683c4ce3939e065162a24d16df188ce.

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ import io.gitlab.arturbosch.detekt.report.ReportMergeTask
 import nmcp.NmcpAggregationExtension
 import nmcp.NmcpExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
-import java.util.concurrent.TimeUnit
 
 plugins {
     base
@@ -67,7 +66,7 @@ allprojects {
     }
     // bluetape4k snapshot 버전 사용 시만 사용하세요.
     configurations.all {
-        resolutionStrategy.cacheChangingModulesFor(0, TimeUnit.SECONDS)
+        resolutionStrategy.cacheChangingModulesFor(1, TimeUnit.DAYS)
     }
 }
 
